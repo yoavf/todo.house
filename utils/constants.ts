c2d7282a3bd9f403e179a6c270c8a95921f6aa59
Yoav Farhi <yoav@farhi.org>
2025-07-17 10:55:08 +0300

    update constants

--- utils/constants.ts
+++ utils/constants.ts
@@ -2,6 +2,6 @@
  * Constants used throughout the app
  */
 
-export const MILLISECONDS_PER_HOUR = 1000 * 60 * 60;
-export const MILLISECONDS_PER_DAY = MILLISECONDS_PER_HOUR * 24;
 export const MILLISECONDS_PER_MINUTE = 1000 * 60;
+export const MILLISECONDS_PER_HOUR = MILLISECONDS_PER_MINUTE * 60;
+export const MILLISECONDS_PER_DAY = MILLISECONDS_PER_HOUR * 24;
